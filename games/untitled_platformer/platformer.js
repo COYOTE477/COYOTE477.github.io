f703e66068f4a6f288578122e3c532f2cb4a54b4
codybargholz <59399957+codybargholz@users.noreply.github.com>
2024-02-23 19:36:03 -0800

    added a player and gravity to the platformer
    SO FUN!

--- games/untitled_platformer/platformer.js
+++ games/untitled_platformer/platformer.js
@@ -1,9 +1,53 @@
-
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
 canvas.width = 64 * 16
 canvas.height = 64 * 9
 //thank you chris courses
-c.fillStyle = 'white'
-c.fillRect(0, 0, canvas.width, canvas.height)
+
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.velocity = {
+            x: 1,
+            y: 1
+        }
+        this.width = 100
+        this.height = 100
+        this.sides = {
+            bottom: this.position.y + this.height
+        }
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+    update() {
+        if (this.sides.bottom < canvas.height) {
+            this.velocity.y++
+            this.position.y += this.velocity.y
+            this.sides.bottom = this.position.y + this.height
+        }
+    }
+
+}
+
+const player = new Player()
+
+// let bottom = y + 100
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.clearRect
+    c.fillStyle = 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    player.draw()
+}
+
+
+animate()
