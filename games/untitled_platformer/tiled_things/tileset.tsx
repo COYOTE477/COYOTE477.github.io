23f31acb013e0fa91a8c54e495a89435eaa8a81c
codybargholz <coolguyerikdb@gmail.com>
2024-02-27 07:23:37 -0800

    idk
    idk

--- games/untitled_platformer/tiled_things/tileset.tsx
+++ games/untitled_platformer/tiled_things/tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tileset" tilewidth="32" tileheight="32" tilecount="15" columns="5">
- <image source="../art/tileset.png" width="160" height="96"/>
+<tileset version="1.10" tiledversion="1.10.2" name="tileset" tilewidth="32" tileheight="32" tilecount="24" columns="6">
+ <image source="../art/tileset.png" width="192" height="128"/>
 </tileset>
